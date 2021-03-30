b3e22a3277e9be384583bae7534087db03b82648
Ulanchan <rizkyapriani149@gmail.com>
2021-03-30 10:08:58 +0700

    projek magang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialDesign} from './material'
+import { MaterialDesign } from './material';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
